fb4216c7f40191dd1a972b27345300518331443e
Nick Brown <nick@mesham.com>
2018-03-02 17:05:55 +0000

    Updated examples

--- examples/src/example_1.c
+++ examples/src/example_1.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 void my_task(EDAT_Event*, int);
+void my_task2(EDAT_Event*, int);
 
 int main(int argc, char * argv[]) {
   edatInit(&argc, &argv);
@@ -10,6 +11,7 @@ int main(int argc, char * argv[]) {
   if (edatGetRank() == 0) {
     edatScheduleTask(my_task, "my_task");
   } else if (edatGetRank() == 1) {
+    edatScheduleTask(my_task2, "my_task2");
     d=33;
     edatFireEvent(&d, EDAT_INT, 1, 0, "my_task");
   }
@@ -19,8 +21,13 @@ int main(int argc, char * argv[]) {
 
 void my_task(EDAT_Event * events, int num_events) {
   if (events[0].metadata.number_elements > 0 && events[0].metadata.data_type == EDAT_INT) {
-    printf("Hello world %d!\n", *((int *) events[0].data));
+    printf("Hello world from %d with data %d!\n", edatGetRank(), *((int *) events[0].data));
   } else {
     printf("Hello world!\n");
   }
+  edatFireEvent(NULL, EDAT_NOTYPE, 0, 1, "my_task2");
+}
+
+void my_task2(EDAT_Event * events, int num_events) {
+  printf("Task two running on %d\n", edatGetRank());
 }
--- examples/src/example_5.c
+++ examples/src/example_5.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 void my_task(EDAT_Event*, int);
+void barrier_task(EDAT_Event* , int);
 
 int main(int argc, char * argv[]) {
   edatInit(&argc, &argv);
@@ -13,14 +14,20 @@ int main(int argc, char * argv[]) {
   } else {
     edatFireEvent(&myval, EDAT_INT, 1, 0, "a");
   }
+  edatScheduleMultiTask(barrier_task, 1, EDAT_ALL, "barrier");
+  edatFireEvent(NULL, EDAT_NOTYPE, 0, EDAT_ALL, "barrier");
   edatFinalise();
   return 0;
 }
 
 void my_task(EDAT_Event * events, int num_events) {
-  printf("Number of events %d\n", num_events);
+  printf("[%d] Number of events %d\n", edatGetRank(), num_events);
   int i=0;
   for (i=0;i<num_events;i++) {
-    printf("Item %d from %d with UUID %s: %d\n", i, events[i].metadata.source, events[i].metadata.unique_id, *((int*) events[i].data));
+    printf("[%d] Item %d from %d with UUID %s: %d\n", edatGetRank(), i, events[i].metadata.source, events[i].metadata.unique_id, *((int*) events[i].data));
   }
+}
+
+void barrier_task(EDAT_Event * events, int num_events) {
+  printf("[%d] Barrier with %d events\n", edatGetRank(), num_events);
 }
