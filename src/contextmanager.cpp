1fb0bd06b3baef4f316c7199d72b61369c7041a5
Nick Brown <nick@mesham.com>
2018-05-02 13:21:13 +0100

    Commenting for context manager

--- src/contextmanager.cpp
+++ src/contextmanager.cpp
@@ -2,12 +2,19 @@
 #include "contextmanager.h"
 #include "misc.h"
 
+/**
+* Adds the definition of a context to the manager, this will also update the unique context definition id that is used as the type
+*/
 int ContextManager::addDefinition(ContextDefinition * definition) {
   definitions.insert(std::pair<int, ContextDefinition*>(definitionId, definition));
   definitionId++;
   return definitionId-1;
 }
 
+/**
+* Creates a specific context based on the type of the definition (that has already been defined.) This will allocate the required
+* memory and then store a pointer to this memory, the idea being that the manager is overall responsible for each of these contexts
+*/
 void* ContextManager::createContext(int contextType) {
   std::map<int, ContextDefinition*>::iterator it = definitions.find(contextType);
   if (it != definitions.end()) {
@@ -18,6 +25,10 @@ void* ContextManager::createContext(int contextType) {
   }
 }
 
+/**
+* Determines whether a specific type is a context type or not (in such case it would be a base type or errornous.) This is mainly
+* for firing events.
+*/
 bool ContextManager::isTypeAContext(int type) {
   if (type >= BASE_CONTEXT_ID) {
     return definitions.count(type) > 0;
@@ -25,6 +36,10 @@ bool ContextManager::isTypeAContext(int type) {
   return false;
 }
 
+/**
+* Retrieves the size of the context payload from the type. Currently this is easy as we just pass a pointer to the context, but
+* in the future will need to be extended most likely.
+*/
 int ContextManager::getContextEventPayloadSize(int contextType) {
   std::map<int, ContextDefinition*>::iterator it = definitions.find(contextType);
   if (it != definitions.end()) {
@@ -35,6 +50,9 @@ int ContextManager::getContextEventPayloadSize(int contextType) {
   }
 }
 
+/**
+* Creates a specific instantiation of a context and stores a pointer to it.
+*/
 void* ContextDefinition::create() {
   char * data=(char*) malloc(numberBytes);
   dataInstances.push_back((void*) data);
