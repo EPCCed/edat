95f917ce51027d8e1dfe6ad589668f615fc7dbc1
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2018-07-24 17:37:22 +0100

    resilience.cpp: added a mutex to protect against where event arrived before external_ledger has been locally instantiated

--- src/resilience.cpp
+++ src/resilience.cpp
@@ -12,6 +12,7 @@
 
 static EDAT_Thread_Ledger * internal_ledger;
 static EDAT_Process_Ledger * external_ledger;
+static std::mutex ledger_mutex;
 
 /**
 * Allocates the two ledgers and notifies the user that resilience is active.
@@ -22,10 +23,12 @@ static EDAT_Process_Ledger * external_ledger;
 * not run.
 */
 void resilienceInit(Scheduler& ascheduler, ThreadPool& athreadpool, Messaging& amessaging, const std::thread::id thread_id) {
+  ledger_mutex.lock();
   int my_rank = amessaging.getRank();
 
   internal_ledger = new EDAT_Thread_Ledger(ascheduler, athreadpool, amessaging, thread_id);
   external_ledger = new EDAT_Process_Ledger(ascheduler, my_rank);
+  ledger_mutex.unlock();
 
   if (!my_rank) {
     std::cout << "EDAT resilience initialised." << std::endl;
@@ -49,6 +52,7 @@ void resilienceTaskScheduled(PendingTaskDescriptor& ptd) {
 */
 void resilienceAddEvent(SpecificEvent& event) {
   DependencyKey depkey = DependencyKey(event.getEventId(), event.getSourcePid());
+  std::lock_guard<std::mutex> lock(ledger_mutex);
   external_ledger->addEvent(depkey, event);
   return;
 }
