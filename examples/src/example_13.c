7f36ca5db0db2d6f93d9cbe3fe83bc3d53eef36e
Nick Brown <nick@mesham.com>
2018-08-02 10:50:54 +0100

    New concurrency control example, removed cholesky example as it was not compiling or completed.

---
+++ examples/src/example_13.c
@@ -0,0 +1,34 @@
+/*
+* This example provides an illustration of using the concurrency control mechanisms of lock, unlock and testlock. Two tasks will run (but the task function), we fire
+* them with different integers just for tracking in the printing out of information. Then each task will do a series of locks, unlocks and testlock. Note that on task
+* completion all locks that are current acquired are automatically released, hence in this case mylock2, even though it is never explicitly unlocked
+* will be implicitly unlocked when the task completes.
+*/
+
+#include <stdio.h>
+#include "edat.h"
+
+static void task(EDAT_Event*, int);
+
+int main() {
+  edatInit();
+  edatScheduleTask(task, 1, EDAT_SELF, "task_num");
+  edatScheduleTask(task, 1, EDAT_SELF, "task_num");
+  int t=0;
+  edatFireEvent(&t, 1, EDAT_INT, EDAT_SELF, "task_num");
+  t=1;
+  edatFireEvent(&t, 1, EDAT_INT, EDAT_SELF, "task_num");
+  edatFinalise();
+	return 0;
+}
+
+static void task(EDAT_Event * events, int numevents) {
+  int task_id=*((int*) events[0].data);
+  edatLock("mylock");
+  printf("[%d] Got lock num one\n", task_id);
+  printf("[%d] I have got the first lock before unlock: %s\n", task_id, edatTestLock("mylock") ? "true" : "false");
+  edatUnlock("mylock");
+  printf("[%d] I have got the first lock after unlock: %s\n", task_id, edatTestLock("mylock") ? "true" : "false");
+  edatLock("mylock2");
+  printf("[%d] Leaving task with mylock2\n", task_id);
+}
